69a7e4551610f66e35bd756d6c20870f1707b7dc
Kevin Tsai <k.tsai92@gmail.com>
2021-06-22 16:06:43 -0400

    added products

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,26 +1,39 @@
 import React from "react";
 import ProductItem from "./ProductItem";
-import withContent from "../withContext";
+import withContext from "../withContext";
 
 const ProductList = props => {
   const { products } = props.context;
 
   return (
     <>
-      <div className= "hero is-primary">
-        <div className= "hero-body container">
+      <div className="hero is-primary">
+        <div className="hero-body container">
           <h4 className="title">Our Products</h4>
         </div>
       </div>
       <br />
-      <div className='container'>
-        <div className= "column columns is-multiline">
-          {/* adds more column elements than you fit in a single row */}
+      <div className="container">
+        <div className="column columns is-multiline">
           {products && products.length ? (
-            
+            products.map((product, index) => (
+              <ProductItem
+                product={product}
+                key={index}
+                addToCart={props.context.addToCart}
+              />
+            ))
+          ) : (
+            <div className="column">
+              <span className="title has-text-grey-light">
+                No products found!
+              </span>
+            </div>
           )}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
+
+export default withContext(ProductList);
